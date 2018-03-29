4e2c5e865ad98c9a84a0658f4cd94aa0dae91d29
jepemo <Jeremias.Perez@desfici.com>
2018-03-29 17:21:00 +0200

    basic add data test

--- tests/test_connection.sh
+++ tests/test_connection.sh
@@ -1,3 +1,5 @@
+#!/bin/bash 
+
 #curl -d '{ "host": "0.0.0.0:8080", "type": "MINER"}' -H "Content-Type: application/json" http://127.0.0.1:8000/connect
 #curl http://127.0.0.1:8000/get_blocks
 #curl http://127.0.0.1:8000/
