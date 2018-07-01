3a543e310145081c0ed15d9882b2d732d71d2001
jepemo <jeremiespm@gmail.com>
2018-07-01 12:46:04 +0200

    new testhttp class

---
+++ tests/test_http_node.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# bbchain - Simple extendable Blockchain implemented in Python
+#
+# Copyright (C) 2017-present Jeremies Pérez Morata
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from bbchain.blockchain import BlockChain
+from bbchain.storage import MemoryDB
+from bbchain.consensus import SimpleConsensus
+
+class TestHttpNode(unittest.TestCase):
+    def default_bc(self):
+        mm = MemoryDB()
+        cons = SimpleConsensus()
+        return BlockChain(mm, cons)
+
+    def setUp(self):
+        print("setup")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_start(self):
+        from bbchain.net.http.node import HttpNode
+        Node = HttpNode
+
+        bc = self.default_bc()
+        node = Node('localhost', '8000', bc, [])
+        #node.start()
